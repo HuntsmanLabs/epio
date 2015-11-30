520bc29dd40e0fc452536d40cc98cfa89d8efe15
Adam Hobson <adamlhobson@gmail.com>
2015-11-30 18:42:54 +1100

    Added API Access

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ActiveRecord::Base
+	belongs_to :user
+	validates_presence_of :content
+	validates_presence_of :scheduled_at
+	validates_length_of :content, maximum: 140, message: "140 characters maximum"
+	validates_datetime :scheduled_at, :on => :create, :on_or_after => Time.zone.now
+
+	def to_twitter
+		client = Twitter::REST::Client.new do |config|
+			config.access_token        = self.user.twitter.oauth_token
+			config.access_token_secret = self.user.twitter.secret
+			config.consumer_key        = ENV['TWITTER_KEY']
+			config.consumer_secret     = ENV['TWITTER_SECRET']	  		
+		end
+		client.update(self.content)
+	end
+end
