137f3a352013881bb59fa9dbeeb936457105ec0c
Adam Hobson <adamlhobson@gmail.com>
2016-01-20 16:45:58 +1100

    Mandrill Mailer Update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-	default from: "no-reply@everpresentapp.com"
+	default from: "Everpresent Team <no-reply@everpresent.io>"
 
 	def welcome_email(user)
 		@user = user
